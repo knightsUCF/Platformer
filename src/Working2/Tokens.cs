2fed937bf71b3486e36d06ee870943d3ab1126da
KnightsUCF <peterk@knights.ucf.edu>
2019-04-16 15:14:19 -0400

    Create Tokens.cs

---
+++ src/Working2/Tokens.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+// add Token script onto a game object called "State", and drag into hierarchy
+
+
+// reference with:
+
+// Tokens tokens;
+// tokens = GameObject.FindObjectOfType<Tokens>();
+// tokens.money;
+
+
+// this is slow and should not be used every frame, if need to use every frame use the singleton pattern
+
+
+
+
+
+
+public class Tokens : MonoBehaviour
+{
+
+
+	public int coins = 0;
+
+
+
+
+
+
+    
+}
